908658aa11d9a750b2dbf221ef95ffb3532b40d6
Brandon-Butterbaugh <bjbutterbaugh@comcast.net>
2025-10-01 14:47:41 -0500

    added player and circles

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import pygame
 
 # importing from other files
 from constants import *
+from player import Player
 
 def main():
     # initialize pygame
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,7 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt += clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
